8154468a4386331ae81f44bec3a4a84104089a10
Demono5060 <demono5060@mail.ru>
2021-09-04 16:51:12 +0700

    little fix of loggin button

--- CTF_Service/backend.py
+++ CTF_Service/backend.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
@@ -13,4 +13,4 @@ def about():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port='777')
