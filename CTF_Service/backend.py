2f0c97d937ab81ac7224d806485ac339c2eb7132
Demono5060 <demono5060@mail.ru>
2021-09-05 14:19:51 +0700

    added sending forms to register and auth

--- CTF_Service/backend.py
+++ CTF_Service/backend.py
@@ -1,5 +1,14 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
+
+
+def login(username, password):
+    print("Username: {} Pass: {}".format(username, password))
+    return redirect(url_for('index'))
+
+
+def add_user(username, password):
+    pass
 
 
 @app.route('/')
@@ -12,14 +21,25 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/auth')
+@app.route('/auth', methods=['GET', 'POST'])
 def auth():
-    return render_template('auth.html')
+    if request.method == 'POST':
+        login(request.form.get('username'), request.form.get('password'))
+        return redirect(url_for('index'))
+    else:
+        return render_template('auth.html')
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    if request.method == 'POST':
+        if request.form.get('password') == request.form.get('cpassword') and request.form.get('cpassword') != '':
+            add_user(request.form.get('username'), request.form.get('password'))
+            login(request.form.get('username'), request.form.get('password'))
+            return redirect(url_for('index'))
+    else:
+        return render_template('register.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port='777')
